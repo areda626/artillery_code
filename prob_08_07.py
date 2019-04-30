c516595d76b4ba2e7b93f53074a4345ef04ca6a0
Alex Reda <sreda1@jhu.edu>
2019-04-30 00:33:13 -0400

    Changed the code so that it would produce the desired set of plots for both Earth and Mars parameters.

--- prob_08_07.py
+++ prob_08_07.py
@@ -3,15 +3,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # constants
-g     = 9.81	# m s^-2
+g = 9.81 # m s^-2
+g_mars  = 3.71	# m s^-2
 m     = 1.0	# kg
-rho   = 1.22	# kg m^-3
+rho_mars   = 0.20	# kg m^-3
+rho = 1.22# kg m^-3
 C     = 0.47	# unitless
 R     = 0.08    # m
 h     = 0.001   # seconds
 theta = 30.0*(np.pi/180) # radians
 v0    = 100.0	# m s^-1
-const = (rho*C*np.pi*R**2)/(2.0*m)
+const_mars = (rho_mars*C*np.pi*R**2)/(2.0*m)
+const = (rho*C*np.pi*R**2)/(2.0*m) 
+
+# define the equations of motion
+def f_mars(r,const):
+    x   = r[0]
+    y   = r[1]
+    vx  = r[2]
+    vy  = r[3]
+    fx  = vx
+    fy  = vy
+    fvx = -const*vx*np.sqrt(vx**2+vy**2)
+    fvy = -g_mars-const*vy*np.sqrt(vx**2+vy**2)
+    return np.array([fx,fy,fvx,fvy],float)
 
 # define the equations of motion
 def f(r,const):
@@ -24,7 +39,6 @@ def f(r,const):
     fvx = -const*vx*np.sqrt(vx**2+vy**2)
     fvy = -g-const*vy*np.sqrt(vx**2+vy**2)
     return np.array([fx,fy,fvx,fvy],float)
-
 # containers for output
 r = np.array([0.0,0.0,v0*np.cos(theta),v0*np.sin(theta)],float)
 xpoints = []
@@ -47,8 +61,28 @@ plt.xlabel("x [m]")
 plt.ylabel('y [m]')
 p1.show()
 
-# try different values of m
+r = np.array([0.0,0.0,v0*np.cos(theta),v0*np.sin(theta)],float)
+xpoints = []
+ypoints = []
+# use fourth-order Runge-Kutta
+while r[1]>=0:
+    k1 = h*f_mars(r,const_mars)
+    k2 = h*f_mars(r+0.5*k1,const_mars)
+    k3 = h*f_mars(r+0.5*k2,const_mars)
+    k4 = h*f_mars(r+k3,const_mars)
+    r += (k1+2*k2+2*k3+k4)/6
+    xpoints.append(r[0])
+    ypoints.append(r[1])
+
+# make part b plot for mars
 p2 = plt.figure(2)
+plt.plot(xpoints,ypoints)
+plt.xlabel("x [m]")
+plt.ylabel('y [m]')
+p2.show()
+
+# try different values of m
+p3 = plt.figure(3)
 for m in [0.25,0.5,1,2,4]:
     const = (rho*C*np.pi*R**2)/(2.0*m)
     r = np.array([0.0,0.0,v0*np.cos(theta),v0*np.sin(theta)],float)
@@ -70,4 +104,29 @@ for m in [0.25,0.5,1,2,4]:
 plt.xlabel("x [m]")
 plt.ylabel('y [m]')
 plt.legend()
-p2.show()
+p3.show()
+
+# try different values of m for mars
+p4= plt.figure(4)
+for m in [0.25,0.5,1,2,4]:
+    const = (rho_mars*C*np.pi*R**2)/(2.0*m)
+    r = np.array([0.0,0.0,v0*np.cos(theta),v0*np.sin(theta)],float)
+    xpoints = []
+    ypoints = []
+
+    # use fourth-order Runge-Kutta
+    while r[1]>=0:
+        k1 = h*f_mars(r,const)
+        k2 = h*f_mars(r+0.5*k1,const)
+        k3 = h*f_mars(r+0.5*k2,const)
+        k4 = h*f_mars(r+k3,const)
+        r += (k1+2*k2+2*k3+k4)/6
+        xpoints.append(r[0])
+        ypoints.append(r[1])
+
+    plt.plot(xpoints,ypoints,label='m = '+str(m)+' kg')
+
+plt.xlabel("x [m]")
+plt.ylabel('y [m]')
+plt.legend()
+p4.show()
